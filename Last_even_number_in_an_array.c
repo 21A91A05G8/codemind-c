c7ed19515c136fe0694f7c6aa4861f8728d7f824
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-04-30 08:26:04 -0700

    #include<stdio.h> int main() {     int i,n,arr[100],p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             p=arr[i];         }     }     printf("%d",p); }

---
+++ Last_even_number_in_an_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,arr[100],p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            p=arr[i];
+        }
+    }
+    printf("%d",p);
+}
