b093dc38ee931817209e1156a7b0c2f20103f428
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-05-01 04:02:37 -0700

    #include<stdio.h> int main() {     int i,j,n,m,arr[100][100],s1=0,s2=0,sum;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(arr[i][j]%2==0)             {                 s2=s2+arr[i][j];             }             if(arr[i][j]%2!=0)             {                 s1=s1+arr[i][j];             }         }     }     sum=s1+s2;     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,m,arr[100][100],s1=0,s2=0,sum;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(arr[i][j]%2==0)
+            {
+                s2=s2+arr[i][j];
+            }
+            if(arr[i][j]%2!=0)
+            {
+                s1=s1+arr[i][j];
+            }
+        }
+    }
+    sum=s1+s2;
+    printf("%d",sum);
+}
