68569c2b67ae6a2585329dc3dcae621494bf5d48
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-05-05 11:10:05 -0700

    #include<stdio.h> int main() {     int n,arr[100],i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }
    }

---
+++ array_of_add_and_even_elements.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+    
+}
