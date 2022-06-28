f5384e9476cf018599b88e649273fd20a63b6ca1
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-28 09:48:44 -0700

    #include<stdio.h> int main() {     int n,arr[1000],i,max;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     max=arr[0];     for(i=0;i<n;i++)     {         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max); }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[1000],i,max;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    max=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+}
