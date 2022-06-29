6fc4f153def5b23f83e6a18a334326e2c1e7b94a
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-29 06:30:59 -0700

    #include<stdio.h> int main() {     int arr[100],i,n,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]<arr[i+1])         {             c++;         }     }     if(c==n-1)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Ascending_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,n,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<arr[i+1])
+        {
+            c++;
+        }
+    }
+    if(c==n-1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
