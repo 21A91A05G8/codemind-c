51401560e830e1fb7f9f6d59df655d35c999744a
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-29 08:39:15 -0700

    #include<stdio.h> int main() {     int arr[100],i,a,b,n,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]<=a && arr[i]>=b || arr[i]>=a && arr[i]<=b)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ sum_of_elements_between_A_and_B.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,a,b,n,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]<=a && arr[i]>=b || arr[i]>=a && arr[i]<=b)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
