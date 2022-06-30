9e81fb5bb1de501b3361c6d8e6edcea7df395659
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-30 11:29:42 -0700

    #include<stdio.h> int main() {     int n,i,arr[100],c=0,k=0,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         k=k+c%2;     }
        printf("%d",k);
    }

---
+++ CLOTHING_STORE.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],c=0,k=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        k=k+c%2;
+    }
+    
+    printf("%d",k);
+}
