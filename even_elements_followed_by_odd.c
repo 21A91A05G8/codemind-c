c1fc4f0944cffeaa5769ab7e2bd456eaa3959f47
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-29 23:33:38 -0700

    #include<stdio.h> int main() {     int arr[100],i,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             printf("%d ",arr[i]);         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             printf("%d ",arr[i]);         }     } }

--- even_elements_followed_by_odd.c
+++ even_elements_followed_by_odd.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,arr[100];
+    int arr[100],i,n;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -16,7 +16,7 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        if(arr[i]%2)
+        if(arr[i]%2!=0)
         {
             printf("%d ",arr[i]);
         }
