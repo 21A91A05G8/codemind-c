6a1515d2efb15da608d2bb3e07ff48138c241732
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-30 07:44:13 -0700

    #include<stdio.h> int main() {     int a,b,arr[100],i,x,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(b<arr[i] || a>arr[i])         {             printf("%d ",arr[i]);             x++;         }     }     if(x==0)     {         printf("-1");     } }

---
+++ elements_not_in_between_A_and_B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,arr[100],i,x,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(b<arr[i] || a>arr[i])
+        {
+            printf("%d ",arr[i]);
+            x++;
+        }
+    }
+    if(x==0)
+    {
+        printf("-1");
+    }
+}
