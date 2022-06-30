6607c8c6a35975af67371a111842bfa280d47089
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-06-30 09:32:18 -0700

    #include<stdio.h> int main() {     int n,i,j,c=0,c1=1;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         c=0;         if(n%i==0)         {             for(j=2;j<n;j++)             {                 if(i%j==0)                 {                     c++;                 }             }         }         if(c>1)         {             c1++;         }     }     printf("%d",c1); }

---
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c=0,c1=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        c=0;
+        if(n%i==0)
+        {
+            for(j=2;j<n;j++)
+            {
+                if(i%j==0)
+                {
+                    c++;
+                }
+            }
+        }
+        if(c>1)
+        {
+            c1++;
+        }
+    }
+    printf("%d",c1);
+}
