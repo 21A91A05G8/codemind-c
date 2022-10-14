2b87dfc4ac4595025600e290331a569829a6fa50
21A91A05G8 <97424432+21A91A05G8@users.noreply.github.com>
2022-10-14 22:00:22 +0530

    #include <stdio.h> #include <string.h>
    int main()
    {
      	char str[100];
      	int i, len;
      	scanf("%[^
    ]s",str);
      	
      	len = strlen(str);
      	for(i = len - 1; i >= 0; i--)
    	{
    		if(str[i] == ' ')
    		{
    			str[i] = NULL;
    			printf("%s ", &(str[i]) + 1);	
    		} 
    	}
    	printf("%s", str);
    	
      	return 0;
    }

--- Reverse_String.c
+++ Reverse_String.c
@@ -1,18 +1,23 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+ 
 int main()
 {
-    char str[100];
-    int len=0,i;
-    scanf("%[^
+  	char str[100];
+  	int i, len;
+  	scanf("%[^
 ]s",str);
-    for(i=0;str[i]!=NULL;i++)
-    {
-        len++;
-    }
-    for(i=len-1;i>=0;i--)
-    {
-        printf("%c",str[i]);
-    }
-    
+  	
+  	len = strlen(str);
+  	for(i = len - 1; i >= 0; i--)
+	{
+		if(str[i] == ' ')
+		{
+			str[i] = NULL;
+			printf("%s ", &(str[i]) + 1);	
+		} 
+	}
+	printf("%s", str);
+	
+  	return 0;
 }
